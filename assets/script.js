0ecbaf8c1381eeeb16890e1ea075e5536bcf9357
Matt Jones <glitchy81@Matts-MBP.lan>
2022-11-11 18:04:16 +0000

    added some javascript to the js file

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// iterate over topics
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+// create a conditional that will switch to whichever 
+// topic we want to focus on!
+
+// if (topic === 'HTML') {
+//     console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//     console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//     console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//     console.log("Let's study JavaScript!");
+
+// } else {
+//     console.log('Please try again')
+// }
+
+
+// array testing
+
+// var shapes = ["triangle", "square", "pentagon", "circle"];
+// console.log(shapes[0]);
+
+// use for loop to iterate over the shapes
+
+// var shapes = ["triangle", "square", "pentagon", "circle"];
+
+// for(var x = 0; x < shapes.length; x++) {
+//  console.log(shapes[x]);
+// }
