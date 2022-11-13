d366f8df7af060f85340ff162bcd33cb4b2d4ed0
Matt Jones <glitchy81@Matts-MacBook-Pro.local>
2022-11-13 11:49:27 +0000

    added a javascript file to the assets folder, under 'scripts', added some information about the basics of JS.

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,44 @@
+
+// variable array to hold topics
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-// iterate over topics
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+// add a random func to choose a topic to study
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+// put the for iteration loop inside a function 
+function listTopics() {
+    // iterate through array
+    for (var x = 0; x < topics.length; x++) {
+        // print to console
+        console.log(topics[x]);
+    }
 }
 
-// create a conditional that will switch to whichever 
-// topic we want to focus on!
+/* create a conditional that will switch to whichever 
+topic we want to focus on!
+wrap this inside a new function */
 
-// if (topic === 'HTML') {
-//     console.log("Let's study HTML!");
-// } else if (topic === 'CSS') {
-//     console.log("Let's study CSS!");
-// } else if (topic === 'Git') {
-//     console.log("Let's study Git!");
-// } else if (topic === 'JavaScript') {
-//     console.log("Let's study JavaScript!");
+function selectTopic() {
+    // accessing an array means topic[0]
+    // NOT 'topics' - returned 'please try again'
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    
+    } else {
+        console.log('Please try again')
+    }
+}
 
-// } else {
-//     console.log('Please try again')
-// }
+// update user with info
+console.log("Here are the topics we learned through Prework:");
+listTopics();
 
-
-// array testing
-
-// var shapes = ["triangle", "square", "pentagon", "circle"];
-// console.log(shapes[0]);
-
-// use for loop to iterate over the shapes
-
-// var shapes = ["triangle", "square", "pentagon", "circle"];
-
-// for(var x = 0; x < shapes.length; x++) {
-//  console.log(shapes[x]);
-// }
+console.log("Which topic should we study first?");
+// call conditional selectTopic
+selectTopic();
